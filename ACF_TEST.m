dee30d8fd253db772066285e2b54acac4f3417a4
조철희 <chjo70@hotmail.com>
2019-07-29 12:23:24 +0900

    ACF 연산 테스트

---
+++ ACF_TEST.m
@@ -0,0 +1,26 @@
+disp('ACF ¿¬»ê½Ä')
+whos
+
+%% dtoa = xlsread( 'acf_data.xlsx', 'B:B' );
+toa = xlsread( 'acf_data.xlsx', 'A:A' );
+
+size_toa = size(toa,1);
+
+for i=1:1:size_toa-1;
+    dtoa(i) = toa(i+1) - toa(i)
+end
+dtoa(i+1) = 0;
+
+%% acf(x,20)
+
+auto_x = autocorr(dtoa, size_toa-2 );
+
+figure
+y = ones( size_toa, 1 );
+plot(toa, dtoa,'-*')
+
+figure
+plot(auto_x,'-*')
+
+%%figure
+%%plot(auto_x,'-*')
