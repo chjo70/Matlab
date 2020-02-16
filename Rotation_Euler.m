6cc5cd91e2f785f47555f36948d7a9984330b13a
조철희 <chjo70@hotmail.com>
2020-02-17 00:23:12 +0900

    수정

--- Rotation_Euler.m
+++ Rotation_Euler.m
@@ -1,3 +1,5 @@
+clear
+
 p = pi / 4;
 y = pi /2;
 r = 0;
@@ -29,33 +31,38 @@ line(2*xl, [0,0], [0,0], 'LineWidth', 3, 'Color', 'b');
 line([0,0], 2*yl, [0,0], 'LineWidth', 3, 'Color', 'b');
 line([0,0], [0,0], 2*zl, 'LineWidth', 3, 'Color', 'b');
 
+X = [ 1; 0; 0 ];
 mArrow3([0 0 0],X, 'color', 'red');
 
-X = [ 1; 0; 0 ];
-
-p1 = -pi / 8;
-y = pi / 4;
+% roll 값은 0 으로 가정한다.
 r = 0;
 
-for y=0 : pi/4 : pi
-    %fprintf( '\n---------------------------------------------' )
-    %fprintf( '\n방위각(YAW)=%.1f도, 고각(PITCH)=%.1f도' , y * 180 / pi, p1 * 180/pi )
+% 0 도에서 180도 변경시 방위각/피치 값
+%A = {'방위', '회전각', '고각' };
+p1 = -10 * pi / 180;
+row = 1;
+for y=0 : pi/180 : pi
+    fprintf( '\n---------------------------------------------' )
+    fprintf( '\n방위각(YAW)=%.1f도, 고각(PITCH)=%.1f도' , y * 180 / pi, p1 * 180/pi )
     
     Y = R_P * R_Y * X;
     Y1 = [ cos(-p1) * cos(y) ; sin(y) ; -sin(-p1)*cos(y) ];
-    %fprintf( '\n단위 벡터 : [%.3f, %.3f, %.3f]' , Y1(1), Y1(2), Y1(3) )
-    %h = mArrow3([0 0 0],Y1,'color', 'red', 'stemWidth', 0.02 );
+    fprintf( '\n단위 벡터 : [%.3f, %.3f, %.3f]' , Y1(1), Y1(2), Y1(3) )
+    h = mArrow3([0 0 0],Y1,'color', 'red', 'stemWidth', 0.02 );
     
     p2 = - asin( sin(-p1) * cos(y) );
     y2 = acos( cos(-p1) * cos(y) / cos(-p2) );
-    %fprintf( '\n방위각(YAW)=%.1f도, 고각(PITCH)=%.1f도' , y2 * 180 / pi, p2 * 180/pi )
+    fprintf( '\n방위각(YAW)=%.1f도, 고각(PITCH)=%.1f도' , y2 * 180 / pi, p2 * 180/pi )
     
     Y2 = [ cos(y2) * cos(-p2) ; sin(y2)*cos(-p2) ; -sin(-p2) ];
-    %fprintf( '\n변환 벡터 : [%.3f, %.3f, %.3f]' , Y2(1), Y2(2), Y2(3) )
-    %h = mArrow3([0 0 0],Y2,'color', 'green');    
+    fprintf( '\n변환 벡터 : [%.3f, %.3f, %.3f]' , Y2(1), Y2(2), Y2(3) )
+    h = mArrow3([0 0 0],Y2,'color', 'green');    
+    A(row,:) = [ y*180/pi, p1*180/pi, y2*180/pi, p2*180/pi ];
+    row = row + 1;
 end    
 
-for y=-pi : pi/4 : 0
+% -180 도에서 0도 변경시 방위각/피치 값
+for y=-pi : pi/180 : 0
     fprintf( '\n---------------------------------------------' )
     fprintf( '\n방위각(YAW)=%.1f도, 고각(PITCH)=%.1f도' , y * 180 / pi, p1 * 180/pi )
     
@@ -70,8 +77,14 @@ for y=-pi : pi/4 : 0
     
     Y2 = [ cos(y2) * cos(-p2) ; sin(y2)*cos(-p2) ; -sin(-p2) ];
     fprintf( '\n변환 벡터 : [%.3f, %.3f, %.3f]' , Y2(1), Y2(2), Y2(3) )
-    h = mArrow3([0 0 0],Y2,'color', 'green');    
+    h = mArrow3([0 0 0],Y2,'color', 'green'); 
+    
+    A(row,:) = [ y*180/pi, p1*180/pi, y2*180/pi, p2*180/pi ];
+    row = row + 1;    
 end    
+
+filename = 'd:\testdata.xlsx';
+xlswrite(filename,A)
 
 fprintf( '\n' )
 
